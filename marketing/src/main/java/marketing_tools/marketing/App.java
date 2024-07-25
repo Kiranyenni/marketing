0169a79404c710b05901729115d9461fa4d263cd
Kiranyenni <kiranyenni@users.noreply.github.com>
2024-07-25 12:21:50 +0530

    hi marketing

---
+++ marketing/src/main/java/marketing_tools/marketing/App.java
@@ -0,0 +1,13 @@
+package marketing_tools.marketing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "HI WELCOME TO MARKETING!" );
+    }
+}
